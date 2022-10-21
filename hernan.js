6ee6148e96008bf1f5444c1d37e1bb0c82350521
HernanGarcia1985 <hernang1985@gmail.com>
2022-10-21 20:21:28 -0300

    promise

--- hernan.js
+++ hernan.js
@@ -1,1 +1,8 @@
-console.log("hernan");
+function presentacion () {
+    console.log("hernan");
+}
+
+let promise = new Promise(function(resolve, reject) {
+    setTimeout(() => resolve("done"), 1000);
+    presentacion();
+  });
