c9ba1b807b9c5b22f0731283b1135d00f09762dc
Emir Moretti <emir.moretti@hotmail.com.ar>
2022-10-28 20:32:33 -0300

    emir folder

---
+++ emir/emir.js
@@ -0,0 +1,11 @@
+console.log("emir");
+
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(() => reject(new Error("err")), 1000);
+});
+
+promise.then(
+    result => alert(result),
+    error => alert(error) 
+);
+console.log("Hello World");
---
+++ emir/src/main.js
@@ -0,0 +1,13 @@
+import Cards from './Cards.svelte';
+
+var app = {};
+
+app.cards = function(html,cardsDatos) {
+	return new Cards({
+		target: html,
+	    props: {cardsDatos},
+	})
+	
+}
+
+export default app;
