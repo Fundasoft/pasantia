c3b8102547bc46670a7da7a2d5977e09af05afcd
juan <juankcba@hotmail.com>
2022-10-28 20:00:26 -0300

    Tareas y sistema

--- juankbyte/public/js/tateti.js
+++ juankbyte/public/js/tateti.js
@@ -1,3 +1,35 @@
-console.log("juank");
+const CRUZ = 'x', CIRCULO = 'o';
 
-console.log("Facundo")
+var tablero = [[0,0,0],[0,0,0],[0,0,0]];
+var turno = CRUZ;
+
+function dibujarTabler(tablero){
+	// biujar el tablero
+	console.log(tablero);
+}
+
+function cambiarTurno(){
+	if(turno==CRUZ){
+		turno = CIRCULO; 
+	}else{
+		turno = CRUZ;
+	}	
+}
+
+// Indica si hay tateti en el tablero
+function hayTateti(tablero){
+	
+}
+
+function finDeJuego(tablero){
+	// utilizar: hayTateti(tablero) + tablero completo?
+}
+
+while (!finDeJuego(tablero)){
+	if (jugar()) {
+		
+	}
+}
+
+console.log('Juego terminado. Ganador', turno);
+
