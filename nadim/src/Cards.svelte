4c9a46b45cc607b782215bb512e4af4a1afee90f
Nadim Aisum <nadincho18@gmail.com>
2022-11-02 19:31:10 -0300

    borre imagenes y titulo agregado

--- nadim/src/Cards.svelte
+++ nadim/src/Cards.svelte
@@ -10,10 +10,12 @@
       <img src="images/logo/womanWorking.png" alt="" style="width: 400px;">
     
       <div class="cardContent">
+        <div class="subtitle">WELCOME TO ACCOUNTING</div>
+        <div class="title">We Are the Best Accounting Agency</div>
     {#each cardsDatos as item}
     <div class="vstack pt-4 d-flex">
         <div class="icon">
-          <i class="{item.iconCard}"></i>
+          <i class="{item.iconCard}" id="cardIcon"></i>
         </div>
         <div class="cardContent">
           <div class="tittle">{item.cardTitle}</div>
