7f8438435573b6ee9a01055cd8f6066b90bdf9e8
Luca <lupattin@gmail.comgit config --global user.email lupattin@gmail.comgit config --global user.email lupattin@gmail.com>
2022-11-03 01:02:24 -0300

    agrego modal

--- luca/src/Cards.svelte
+++ luca/src/Cards.svelte
@@ -1,14 +1,22 @@
 <script>
 
      export let cardsDatos;
-
-</script>
+     
+</script> 
 
 
   <div class="row g-0">
     {#each cardsDatos as item}
     <div class=" col-6 col-xxl-3 col-md-4">
-      <div class="container contenedor h-100">
+      <div class="container contenedor h-100" data-bs-toggle="modal" data-bs-target="#{item.id}">
+        <div class="modal fade" id={item.id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <img src={item.url} class="img-fluid" style="height:400px" alt="...">
+              <p class="text-uppercase imagenes">{item.title}</p>
+            </div>
+          </div>
+        </div>
         <img src={item.url} class="img-fluid" style="height:400px" alt="...">
         <p class="text-uppercase imagenes">{item.title}</p>
       </div>
