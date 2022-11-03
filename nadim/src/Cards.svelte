3886793f481fe45c8b421ab2cae5174dbfde9af5
Nadim18-lab <nadincho18@gmail.com>
2022-11-03 11:07:12 -0300

    responsive with col and row

--- nadim/src/Cards.svelte
+++ nadim/src/Cards.svelte
@@ -4,27 +4,30 @@
 
 </script>
 
+
+
 <div class="container-fluid mt-5">
-  <div class="row bg-light">
-    <div class="allContent">
-      <img src="images/logo/womanWorking.png" alt="" style="width: 400px;">
-    
-      <div class="cardContent">
-        <div class="subtitle">WELCOME TO ACCOUNTING</div>
-        <div class="title">We Are the Best Accounting Agency</div>
+  <div class="row d-flex bg-light">
+    <div class="col-sm-8 d-flex justify-content-center">
+      <img src="images/logo/womanWorking.png" alt="" style="width: 70%;">
+    </div>
+    <div class="col md-0" width="width: 70%;">
+      <div class="subtitle">WELCOME TO ACCOUNTING</div>
+      <div class="title">We Are the Best Accounting Agency</div>
     {#each cardsDatos as item}
-    <div class="vstack pt-4 d-flex">
-        <div class="icon">
-          <i class="{item.iconCard}" id="cardIcon"></i>
-        </div>
-        <div class="cardContent">
-          <div class="tittle">{item.cardTitle}</div>
-          <div class="description">{item.cardSubTitle}</div>
-        </div>
+      <div class="col pt-4 md-auto">
+          <div class="icon">
+            <i class="{item.iconCard}" id="cardIcon"></i>
+          </div>
+          <div class="cardContent">
+            <div class="tittle">{item.cardTitle}</div>
+            <div class="description">{item.cardSubTitle}</div>
+          </div>
     </div>
     {/each}
   </div>
   </div>
-
-  </div>
 </div>
+
+
+
